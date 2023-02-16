6b30c80ddfcc9a8688771889e94d6a86dba58329
anushakaverii <anushakaverii@gmail.com>
2023-02-16 17:37:42 -0600

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,72 @@
+module.exports = {
+  john: [
+    {
+      date: "1/1/2023",
+      amount: 120
+    },
+    {
+      date: "2/1/2023",
+      amount: 75
+    },
+    {
+      date: "3/1/2023",
+      amount: 94
+    },
+    {
+      date: "1/1/2023",
+      amount: 10
+    },
+    {
+      date: "1/1/2023",
+      amount: 75
+    },
+    {
+      date: "3/1/2023",
+      amount: 200
+    },
+    {
+      date: "2/1/2023",
+      amount: 1
+    },
+    {
+      date: "3/1/2023",
+      amount: 80
+    },
+    {
+      date: "3/1/2023",
+      amount: 224
+    }
+  ],
+  paul: [
+    {
+      date: "1/1/2023",
+      amount: 125
+    },
+    {
+      date: "2/28/2023",
+      amount: 75
+    },
+    {
+      date: "3/15/2023",
+      amount: 10
+    },
+    {
+      date: "2/11/2023",
+      amount: 75
+    },
+    {
+      date: "3/24/2023",
+      amount: 200
+    },
+    {
+      date: "2/2/2023",
+      amount: 224
+    },
+      {
+       date: "2/2/2023",
+       amount: 120
+     },
+  ],
+ 
+};
+
